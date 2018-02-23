ba616e42471cdd827812017fcd989753b28e3b23
maheshwarishikha <maheshwari.shikha@gmail.com>
2018-02-23 05:42:51 +0000

    Add files via upload

--- setup_blockchainNetwork.sh
+++ setup_blockchainNetwork.sh
@@ -34,9 +34,11 @@ sleep 5
 pod=$(kubectl get pods  --show-all --selector=job-name=copyartifacts --output=jsonpath={.items..metadata.name})
 kubectl cp ./artifacts $pod:/shared
 
+echo "Waiting for 10 more seconds for copying artifacts to avoid any network delay"
+sleep 10
 JOBSTATUS=$(kubectl get jobs |grep "copyartifacts" |awk '{print $3}')
 while [ "${JOBSTATUS}" != "1" ]; do
-    echo "Waiting for copyartifacts Job to complete"
+    echo "Waiting for copyartifacts job to complete"
     sleep 1;
     PODSTATUS=$(kubectl get pods --show-all| grep "copyartifacts" | awk '{print $3}')
         if [ "${PODSTATUS}" == "Error" ]; then
@@ -45,6 +47,7 @@ while [ "${JOBSTATUS}" != "1" ]; do
         fi
     JOBSTATUS=$(kubectl get jobs |grep "copyartifacts" |awk '{print $3}')
 done
+echo "Copy artifacts job completed"
 
 
 echo -e "\nGenerating the required artifacts for Blockchain network"
@@ -53,7 +56,7 @@ kubectl create -f ${KUBECONFIG_FOLDER}/generateArtifactsJob.yaml
 
 JOBSTATUS=$(kubectl get jobs |grep utils|awk '{print $3}')
 while [ "${JOBSTATUS}" != "1" ]; do
-    echo "Waiting for generateArtifactsJob to complete"
+    echo "Waiting for generateArtifacts job to complete"
     sleep 1;
     # UTILSLEFT=$(kubectl get pods --show-all | grep utils | awk '{print $2}')
     UTILSSTATUS=$(kubectl get pods --show-all | grep "utils" | awk '{print $3}')
@@ -91,70 +94,67 @@ echo -e "\nCreating channel transaction artifact and a channel"
 echo "Running: kubectl create -f ${KUBECONFIG_FOLDER}/create_channel.yaml"
 kubectl create -f ${KUBECONFIG_FOLDER}/create_channel.yaml
 
-while [ "$(kubectl get pods --show-all| grep createchannel | awk '{print $3}')" != "Completed" ]; do
-    echo "Waiting for createchannel Job to be completed"
+JOBSTATUS=$(kubectl get jobs |grep createchannel |awk '{print $3}')
+while [ "${JOBSTATUS}" != "1" ]; do
+    echo "Waiting for createchannel job to be completed"
     sleep 1;
     if [ "$(kubectl get pods --show-all| grep createchannel | awk '{print $3}')" == "Error" ]; then
         echo "Create Channel Failed"
-        exit
+        exit 1
     fi
+    JOBSTATUS=$(kubectl get jobs |grep createchannel |awk '{print $3}')
 done
 echo "Create Channel Completed Successfully"
 
-
-# echo "Preparing yaml for joinchannel pod"
-# sed -e "s/%PEER_ADDRESS%/${PEER_ADDRESS}/g" -e "s/%CHANNEL_NAME%/${CHANNEL_NAME}/g" -e "s/%PEER_MSPID%/${PEER_MSPID}/g" -e "s|%MSP_CONFIGPATH%|${MSP_CONFIGPATH}|g" ${KUBECONFIG_FOLDER}/join_channel.yaml.base > ${KUBECONFIG_FOLDER}/join_channel.yaml
 
 echo -e "\nCreating joinchannel job"
 echo "Running: kubectl create -f ${KUBECONFIG_FOLDER}/join_channel.yaml"
 kubectl create -f ${KUBECONFIG_FOLDER}/join_channel.yaml
 
-while [ "$(kubectl get pods --show-all| grep joinchannel | awk '{print $3}')" != "Completed" ]; do
-    echo "Waiting for joinchannel container to be Completed"
+JOBSTATUS=$(kubectl get jobs |grep joinchannel |awk '{print $3}')
+while [ "${JOBSTATUS}" != "1" ]; do
+    echo "Waiting for joinchannel job to be completed"
     sleep 1;
     if [ "$(kubectl get pods --show-all| grep joinchannel | awk '{print $3}')" == "Error" ]; then
         echo "Join Channel Failed"
-        exit
+        exit 1
     fi
+    JOBSTATUS=$(kubectl get jobs |grep joinchannel |awk '{print $3}')
 done
 echo "Join Channel Completed Successfully"
+
 
 echo -e "\nCreating installchaincode job"
 echo "Running: kubectl create -f ${KUBECONFIG_FOLDER}/chaincode_install.yaml"
 kubectl create -f ${KUBECONFIG_FOLDER}/chaincode_install.yaml
 
-while [ "$(kubectl get pods --show-all| grep chaincodeinstall | awk '{print $3}')" != "Completed" ]; do
-    echo "Waiting for chaincode_install container to be Completed"
+JOBSTATUS=$(kubectl get jobs |grep chaincodeinstall |awk '{print $3}')
+while [ "${JOBSTATUS}" != "1" ]; do
+    echo "Waiting for chaincodeinstall job to be completed"
     sleep 1;
     if [ "$(kubectl get pods --show-all| grep chaincodeinstall | awk '{print $3}')" == "Error" ]; then
         echo "Chaincode Install Failed"
-        exit
+        exit 1
     fi
+    JOBSTATUS=$(kubectl get jobs |grep chaincodeinstall |awk '{print $3}')
 done
 echo "Chaincode Install Completed Successfully"
+
 
 echo -e "\nCreating chaincodeinstantiate job"
 echo "Running: kubectl create -f ${KUBECONFIG_FOLDER}/chaincode_instantiate.yaml"
 kubectl create -f ${KUBECONFIG_FOLDER}/chaincode_instantiate.yaml
 
-while [ "$(kubectl get pods --show-all| grep chaincodeinstantiate | awk '{print $3}')" != "Completed" ]; do
-    echo "Waiting for chaincodeinstantiate container to be Completed"
+JOBSTATUS=$(kubectl get jobs |grep chaincodeinstantiate |awk '{print $3}')
+while [ "${JOBSTATUS}" != "1" ]; do
+    echo "Waiting for chaincodeinstantiate job to be completed"
     sleep 1;
     if [ "$(kubectl get pods --show-all| grep chaincodeinstantiate | awk '{print $3}')" == "Error" ]; then
         echo "Chaincode Instantiation Failed"
-        exit
+        exit 1
     fi
+    JOBSTATUS=$(kubectl get jobs |grep chaincodeinstantiate |awk '{print $3}')
 done
 echo "Chaincode Instantiation Completed Successfully"
+sleep 15
 echo -e "\nNetwork Setup Completed !!"
-# echo "=> CREATE_ALL: Running Install Chaincode on Org1 Peer1"
-# CHAINCODE_NAME="example02" CHAINCODE_VERSION="v1" MSP_CONFIGPATH="/shared/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp"  PEER_MSPID="Org1MSP" PEER_ADDRESS="blockchain-org1peer1:30110" create/chaincode_install.sh
-
-# echo ""
-# echo "=> CREATE_ALL: Running Install Chaincode on Org2 Peer1"
-# CHAINCODE_NAME="example02" CHAINCODE_VERSION="v1" MSP_CONFIGPATH="/shared/crypto-config/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp"  PEER_MSPID="Org2MSP" PEER_ADDRESS="blockchain-org2peer1:30210" create/chaincode_install.sh
-
-# echo ""
-# echo "=> CREATE_ALL: Running instantiate chaincode on channel \"channel1\" using \"Org1MSP\""
-# CHANNEL_NAME="channel1" CHAINCODE_NAME="example02" CHAINCODE_VERSION="v1" MSP_CONFIGPATH="/shared/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp"  PEER_MSPID="Org1MSP" PEER_ADDRESS="blockchain-org1peer1:30110" create/chaincode_instantiate.sh
-
