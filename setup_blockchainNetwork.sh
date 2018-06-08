c56f564a281e67093c9068301408411268ddc9cf
Dean McRobie <dean.mcrobie@gmail.com>
2018-06-08 15:40:52 -0400

    added namespacing options

--- setup_blockchainNetwork.sh
+++ setup_blockchainNetwork.sh
@@ -7,25 +7,40 @@ else
     exit
 fi
 
+NAMESPACE_OPT=""
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+    echo "key $key"
+    case $key in
+        -n|--namespace)
+        NAMESPACE_OPT="-n $2"
+        kubectl create namespace $2
+        shift  # past opt
+        shift  # pas namespace
+        # this is okay if it fails because the namespace exists
+        ;;
+    esac
+done
 
 # Creating Persistant Volume
 echo -e "\nCreating volume"
-if [ "$(kubectl get pvc | grep shared-pvc | awk '{print $2}')" != "Bound" ]; then
+if [ "$(kubectl $NAMESPACE_OPT $NAMESPACE_OPT get pvc | grep shared-pvc | awk '{print $2}')" != "Bound" ]; then
     echo "The Persistant Volume does not seem to exist or is not bound"
     echo "Creating Persistant Volume"
 
     if [ "$1" == "--paid" ]; then
         echo "You passed argument --paid. Make sure you have an IBM Cloud Kubernetes - Standard tier. Else, remove --paid option"
-        echo "Running: kubectl create -f ${KUBECONFIG_FOLDER}/createVolume-paid.yaml"
-        kubectl create -f ${KUBECONFIG_FOLDER}/createVolume-paid.yaml
+        echo "Running: kubectl $NAMESPACE_OPT create -f ${KUBECONFIG_FOLDER}/createVolume-paid.yaml"
+        kubectl $NAMESPACE_OPT create -f ${KUBECONFIG_FOLDER}/createVolume-paid.yaml
         sleep 5
     else
-        echo "Running: kubectl create -f ${KUBECONFIG_FOLDER}/createVolume.yaml"
-        kubectl create -f ${KUBECONFIG_FOLDER}/createVolume.yaml
+        echo "Running: kubectl $NAMESPACE_OPT create -f ${KUBECONFIG_FOLDER}/createVolume.yaml"
+        kubectl $NAMESPACE_OPT create -f ${KUBECONFIG_FOLDER}/createVolume.yaml
         sleep 5
     fi
 
-    if [ "kubectl get pvc | grep shared-pvc | awk '{print $3}'" != "shared-pv" ]; then
+    if [ "kubectl $NAMESPACE_OPT get pvc | grep shared-pvc | awk '{print $3}'" != "shared-pv" ]; then
         echo "Success creating Persistant Volume"
     else
         echo "Failed to create Persistant Volume"
@@ -36,12 +51,12 @@ fi
 
 # Copy the required files(configtx.yaml, cruypto-config.yaml, sample chaincode etc.) into volume
 echo -e "\nCreating Copy artifacts job."
-echo "Running: kubectl create -f ${KUBECONFIG_FOLDER}/copyArtifactsJob.yaml"
-kubectl create -f ${KUBECONFIG_FOLDER}/copyArtifactsJob.yaml
+echo "Running: kubectl $NAMESPACE_OPT create -f ${KUBECONFIG_FOLDER}/copyArtifactsJob.yaml"
+kubectl $NAMESPACE_OPT create -f ${KUBECONFIG_FOLDER}/copyArtifactsJob.yaml
 
-pod=$(kubectl get pods  --show-all --selector=job-name=copyartifacts --output=jsonpath={.items..metadata.name})
+pod=$(kubectl $NAMESPACE_OPT get pods  --show-all --selector=job-name=copyartifacts --output=jsonpath={.items..metadata.name})
 
-podSTATUS=$(kubectl get pods  --show-all --selector=job-name=copyartifacts --output=jsonpath={.items..phase})
+podSTATUS=$(kubectl $NAMESPACE_OPT get pods  --show-all --selector=job-name=copyartifacts --output=jsonpath={.items..phase})
 
 while [ "${podSTATUS}" != "Running" ]; do
     echo "Wating for container of copy artifact pod to run. Current status of ${pod} is ${podSTATUS}"
@@ -50,68 +65,68 @@ while [ "${podSTATUS}" != "Running" ]; do
         echo "There is an error in copyartifacts job. Please check logs."
         exit 1
     fi
-    podSTATUS=$(kubectl get pods --show-all --selector=job-name=copyartifacts --output=jsonpath={.items..phase})
+    podSTATUS=$(kubectl $NAMESPACE_OPT get pods --show-all --selector=job-name=copyartifacts --output=jsonpath={.items..phase})
 done
 
 echo -e "${pod} is now ${podSTATUS}"
 echo -e "\nStarting to copy artifacts in persistent volume."
 
 #fix for this script to work on icp and ICS
-kubectl cp ./artifacts $pod:/shared/
+kubectl $NAMESPACE_OPT cp ./artifacts $pod:/shared/
 
 echo "Waiting for 10 more seconds for copying artifacts to avoid any network delay"
 sleep 10
-JOBSTATUS=$(kubectl get jobs |grep "copyartifacts" |awk '{print $3}')
+JOBSTATUS=$(kubectl $NAMESPACE_OPT get jobs |grep "copyartifacts" |awk '{print $3}')
 while [ "${JOBSTATUS}" != "1" ]; do
     echo "Waiting for copyartifacts job to complete"
     sleep 1;
-    PODSTATUS=$(kubectl get pods --show-all| grep "copyartifacts" | awk '{print $3}')
+    PODSTATUS=$(kubectl $NAMESPACE_OPT get pods --show-all| grep "copyartifacts" | awk '{print $3}')
         if [ "${PODSTATUS}" == "Error" ]; then
             echo "There is an error in copyartifacts job. Please check logs."
             exit 1
         fi
-    JOBSTATUS=$(kubectl get jobs |grep "copyartifacts" |awk '{print $3}')
+    JOBSTATUS=$(kubectl $NAMESPACE_OPT get jobs |grep "copyartifacts" |awk '{print $3}')
 done
 echo "Copy artifacts job completed"
 
 
 # Generate Network artifacts using configtx.yaml and crypto-config.yaml
 echo -e "\nGenerating the required artifacts for Blockchain network"
-echo "Running: kubectl create -f ${KUBECONFIG_FOLDER}/generateArtifactsJob.yaml"
-kubectl create -f ${KUBECONFIG_FOLDER}/generateArtifactsJob.yaml
+echo "Running: kubectl $NAMESPACE_OPT create -f ${KUBECONFIG_FOLDER}/generateArtifactsJob.yaml"
+kubectl $NAMESPACE_OPT create -f ${KUBECONFIG_FOLDER}/generateArtifactsJob.yaml
 
-JOBSTATUS=$(kubectl get jobs |grep utils|awk '{print $3}')
+JOBSTATUS=$(kubectl $NAMESPACE_OPT get jobs |grep utils|awk '{print $3}')
 while [ "${JOBSTATUS}" != "1" ]; do
     echo "Waiting for generateArtifacts job to complete"
     sleep 1;
-    # UTILSLEFT=$(kubectl get pods --show-all | grep utils | awk '{print $2}')
-    UTILSSTATUS=$(kubectl get pods --show-all | grep "utils" | awk '{print $3}')
+    # UTILSLEFT=$(kubectl $NAMESPACE_OPT get pods --show-all | grep utils | awk '{print $2}')
+    UTILSSTATUS=$(kubectl $NAMESPACE_OPT get pods --show-all | grep "utils" | awk '{print $3}')
     if [ "${UTILSSTATUS}" == "Error" ]; then
             echo "There is an error in utils job. Please check logs."
             exit 1
     fi
-    # UTILSLEFT=$(kubectl get pods --show-all | grep utils | awk '{print $2}')
-    JOBSTATUS=$(kubectl get jobs |grep utils|awk '{print $3}')
+    # UTILSLEFT=$(kubectl $NAMESPACE_OPT get pods --show-all | grep utils | awk '{print $2}')
+    JOBSTATUS=$(kubectl $NAMESPACE_OPT get jobs |grep utils|awk '{print $3}')
 done
 
 
 # Create services for all peers, ca, orderer
 echo -e "\nCreating Services for blockchain network"
-echo "Running: kubectl create -f ${KUBECONFIG_FOLDER}/blockchain-services.yaml"
-kubectl create -f ${KUBECONFIG_FOLDER}/blockchain-services.yaml
+echo "Running: kubectl $NAMESPACE_OPT create -f ${KUBECONFIG_FOLDER}/blockchain-services.yaml"
+kubectl $NAMESPACE_OPT create -f ${KUBECONFIG_FOLDER}/blockchain-services.yaml
 
 
 # Create peers, ca, orderer using Kubernetes Deployments
 echo -e "\nCreating new Deployment to create four peers in network"
-echo "Running: kubectl create -f ${KUBECONFIG_FOLDER}/peersDeployment.yaml"
-kubectl create -f ${KUBECONFIG_FOLDER}/peersDeployment.yaml
+echo "Running: kubectl $NAMESPACE_OPT create -f ${KUBECONFIG_FOLDER}/peersDeployment.yaml"
+kubectl $NAMESPACE_OPT create -f ${KUBECONFIG_FOLDER}/peersDeployment.yaml
 
 echo "Checking if all deployments are ready"
 
-NUMPENDING=$(kubectl get deployments | grep blockchain | awk '{print $5}' | grep 0 | wc -l | awk '{print $1}')
+NUMPENDING=$(kubectl $NAMESPACE_OPT get deployments | grep blockchain | awk '{print $5}' | grep 0 | wc -l | awk '{print $1}')
 while [ "${NUMPENDING}" != "0" ]; do
     echo "Waiting on pending deployments. Deployments pending = ${NUMPENDING}"
-    NUMPENDING=$(kubectl get deployments | grep blockchain | awk '{print $5}' | grep 0 | wc -l | awk '{print $1}')
+    NUMPENDING=$(kubectl $NAMESPACE_OPT get deployments | grep blockchain | awk '{print $5}' | grep 0 | wc -l | awk '{print $1}')
     sleep 1
 done
 
@@ -121,72 +136,72 @@ sleep 15
 
 # Generate channel artifacts using configtx.yaml and then create channel
 echo -e "\nCreating channel transaction artifact and a channel"
-echo "Running: kubectl create -f ${KUBECONFIG_FOLDER}/create_channel.yaml"
-kubectl create -f ${KUBECONFIG_FOLDER}/create_channel.yaml
+echo "Running: kubectl $NAMESPACE_OPT create -f ${KUBECONFIG_FOLDER}/create_channel.yaml"
+kubectl $NAMESPACE_OPT create -f ${KUBECONFIG_FOLDER}/create_channel.yaml
 
-JOBSTATUS=$(kubectl get jobs |grep createchannel |awk '{print $3}')
+JOBSTATUS=$(kubectl $NAMESPACE_OPT get jobs |grep createchannel |awk '{print $3}')
 while [ "${JOBSTATUS}" != "1" ]; do
     echo "Waiting for createchannel job to be completed"
     sleep 1;
-    if [ "$(kubectl get pods --show-all| grep createchannel | awk '{print $3}')" == "Error" ]; then
+    if [ "$(kubectl $NAMESPACE_OPT get pods --show-all| grep createchannel | awk '{print $3}')" == "Error" ]; then
         echo "Create Channel Failed"
         exit 1
     fi
-    JOBSTATUS=$(kubectl get jobs |grep createchannel |awk '{print $3}')
+    JOBSTATUS=$(kubectl $NAMESPACE_OPT get jobs |grep createchannel |awk '{print $3}')
 done
 echo "Create Channel Completed Successfully"
 
 
 # Join all peers on a channel
 echo -e "\nCreating joinchannel job"
-echo "Running: kubectl create -f ${KUBECONFIG_FOLDER}/join_channel.yaml"
-kubectl create -f ${KUBECONFIG_FOLDER}/join_channel.yaml
+echo "Running: kubectl $NAMESPACE_OPT create -f ${KUBECONFIG_FOLDER}/join_channel.yaml"
+kubectl $NAMESPACE_OPT create -f ${KUBECONFIG_FOLDER}/join_channel.yaml
 
-JOBSTATUS=$(kubectl get jobs |grep joinchannel |awk '{print $3}')
+JOBSTATUS=$(kubectl $NAMESPACE_OPT get jobs |grep joinchannel |awk '{print $3}')
 while [ "${JOBSTATUS}" != "1" ]; do
     echo "Waiting for joinchannel job to be completed"
     sleep 1;
-    if [ "$(kubectl get pods --show-all| grep joinchannel | awk '{print $3}')" == "Error" ]; then
+    if [ "$(kubectl $NAMESPACE_OPT get pods --show-all| grep joinchannel | awk '{print $3}')" == "Error" ]; then
         echo "Join Channel Failed"
         exit 1
     fi
-    JOBSTATUS=$(kubectl get jobs |grep joinchannel |awk '{print $3}')
+    JOBSTATUS=$(kubectl $NAMESPACE_OPT get jobs |grep joinchannel |awk '{print $3}')
 done
 echo "Join Channel Completed Successfully"
 
 
 # Install chaincode on each peer
 echo -e "\nCreating installchaincode job"
-echo "Running: kubectl create -f ${KUBECONFIG_FOLDER}/chaincode_install.yaml"
-kubectl create -f ${KUBECONFIG_FOLDER}/chaincode_install.yaml
+echo "Running: kubectl $NAMESPACE_OPT create -f ${KUBECONFIG_FOLDER}/chaincode_install.yaml"
+kubectl $NAMESPACE_OPT create -f ${KUBECONFIG_FOLDER}/chaincode_install.yaml
 
-JOBSTATUS=$(kubectl get jobs |grep chaincodeinstall |awk '{print $3}')
+JOBSTATUS=$(kubectl $NAMESPACE_OPT get jobs |grep chaincodeinstall |awk '{print $3}')
 while [ "${JOBSTATUS}" != "1" ]; do
     echo "Waiting for chaincodeinstall job to be completed"
     sleep 1;
-    if [ "$(kubectl get pods --show-all| grep chaincodeinstall | awk '{print $3}')" == "Error" ]; then
+    if [ "$(kubectl $NAMESPACE_OPT get pods --show-all| grep chaincodeinstall | awk '{print $3}')" == "Error" ]; then
         echo "Chaincode Install Failed"
         exit 1
     fi
-    JOBSTATUS=$(kubectl get jobs |grep chaincodeinstall |awk '{print $3}')
+    JOBSTATUS=$(kubectl $NAMESPACE_OPT get jobs |grep chaincodeinstall |awk '{print $3}')
 done
 echo "Chaincode Install Completed Successfully"
 
 
 # Instantiate chaincode on channel
 echo -e "\nCreating chaincodeinstantiate job"
-echo "Running: kubectl create -f ${KUBECONFIG_FOLDER}/chaincode_instantiate.yaml"
-kubectl create -f ${KUBECONFIG_FOLDER}/chaincode_instantiate.yaml
+echo "Running: kubectl $NAMESPACE_OPT create -f ${KUBECONFIG_FOLDER}/chaincode_instantiate.yaml"
+kubectl $NAMESPACE_OPT create -f ${KUBECONFIG_FOLDER}/chaincode_instantiate.yaml
 
-JOBSTATUS=$(kubectl get jobs |grep chaincodeinstantiate |awk '{print $3}')
+JOBSTATUS=$(kubectl $NAMESPACE_OPT get jobs |grep chaincodeinstantiate |awk '{print $3}')
 while [ "${JOBSTATUS}" != "1" ]; do
     echo "Waiting for chaincodeinstantiate job to be completed"
     sleep 1;
-    if [ "$(kubectl get pods --show-all| grep chaincodeinstantiate | awk '{print $3}')" == "Error" ]; then
+    if [ "$(kubectl $NAMESPACE_OPT get pods --show-all| grep chaincodeinstantiate | awk '{print $3}')" == "Error" ]; then
         echo "Chaincode Instantiation Failed"
         exit 1
     fi
-    JOBSTATUS=$(kubectl get jobs |grep chaincodeinstantiate |awk '{print $3}')
+    JOBSTATUS=$(kubectl $NAMESPACE_OPT get jobs |grep chaincodeinstantiate |awk '{print $3}')
 done
 echo "Chaincode Instantiation Completed Successfully"
 
